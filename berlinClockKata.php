d83955be54d6100e2e2e52818eff7c6936840454
Loic <loic.thomas@student.vinci.be>
2021-11-24 14:39:26 +0100

    premiere fonction

--- berlinClockKata.php
+++ berlinClockKata.php
@@ -3,4 +3,9 @@
 class berlinClockKata
 {
 
+
+    public function ligne_5_heure(int $nbrEmpoule){
+
+        return $nbrEmpoule * 5;
+    }
 }
--- berlinClockKataTest.php
+++ berlinClockKataTest.php
@@ -1,9 +1,24 @@
 <?php
-
+require_once("berlinClockKata.php");
 
 use PHPUnit\Framework\TestCase;
 
 class berlinClockKataTest extends TestCase
 {
+
+
+    public function testLigne5Heure() : void{
+        $berlinClockKata = new berlinClockKata();
+        $result = $berlinClockKata->ligne_5_heure(0);
+        $this->assertEquals(0, $result);
+        $result = $berlinClockKata->ligne_5_heure(1);
+        $this->assertEquals(5, $result);
+        $result = $berlinClockKata->ligne_5_heure(2);
+        $this->assertEquals(10, $result);
+        $result = $berlinClockKata->ligne_5_heure(3);
+        $this->assertEquals(15, $result);
+        $result = $berlinClockKata->ligne_5_heure(4);
+        $this->assertEquals(20, $result);
+    }
 
 }
